371b34d04c3036e29d54868a3f30c100e71b329a
PATILYASHH <149749700+PATILYASHH@users.noreply.github.com>
2024-01-22 16:07:50 +0000

    this code for Mathematical tables of any number

--- main.c
+++ main.c
@@ -1,7 +1,20 @@
 #include <stdio.h>
 int main() {
 
+int a ;
+// first give access to get input from user
+printf("Enter a number : ");
+scanf("%d",&a);
 
+  // code for multiplication table 
+ printf("Table of %d : \n",a);
+ 
+  for(int i=1; i<=10; i++) 
+    {
+      printf("%d*%d = %d \n",i,a,a*i);
+    
+  
+    }
   
   return 0;
 }
